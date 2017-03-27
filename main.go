ed3a7924baad878ceb32223e2948ca7c386c40a3
Kenta Nakai <kenta.nakai@urouro.net>
2017-03-27 22:37:06 +0900

    List card names in "Do" lists

--- main.go
+++ main.go
@@ -4,15 +4,51 @@ import (
 	"fmt"
 	"github.com/VojtechVitek/go-trello"
 	"log"
+	"os"
 )
 
 func main() {
-	appKey := "application-key"
-	token := "token"
+	appKey := os.Getenv("TRELLO_DEVELOPER_PUBLIC_KEY")
+	token := os.Getenv("TRELLO_MEMBER_TOKEN")
 	trello, err := trello.NewAuthClient(appKey, &token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(trello)
+	user, err := trello.Member("me")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	boards, err := user.Boards()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, b := range boards {
+		if b.Closed {
+			continue
+		}
+
+		lists, err := b.Lists()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, l := range lists {
+			section := false
+
+			if l.Name == "Do" {
+				cards, _ := l.Cards()
+				for _, c := range cards {
+					if section == false {
+						fmt.Println(b.Name)
+						section = true
+					}
+
+					fmt.Println("- " + c.Name)
+				}
+			}
+		}
+	}
 }
