4aa2af6cfebaab16760437bfd04e6a337f455f85
Kenta Nakai <kenta.nakai@urouro.net>
2017-03-28 21:44:56 +0900

    cli-ized

--- main.go
+++ main.go
@@ -3,26 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/VojtechVitek/go-trello"
+	"github.com/urfave/cli"
 	"log"
 	"os"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "trello-fetcher"
+	app.Usage = ""
+	app.Action = current
+
+	app.Run(os.Args)
+}
+
+func current(c *cli.Context) error {
 	appKey := os.Getenv("TRELLO_DEVELOPER_PUBLIC_KEY")
 	token := os.Getenv("TRELLO_MEMBER_TOKEN")
 	trello, err := trello.NewAuthClient(appKey, &token)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 
 	user, err := trello.Member("me")
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 
 	boards, err := user.Boards()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 
 	for _, b := range boards {
@@ -33,12 +46,14 @@ func main() {
 		lists, err := b.Lists()
 		if err != nil {
 			log.Fatal(err)
+			return err
 		}
 
 		for _, l := range lists {
 			section := false
 
 			if l.Name == "Do" {
+
 				cards, _ := l.Cards()
 				for _, c := range cards {
 					if section == false {
@@ -51,4 +66,6 @@ func main() {
 			}
 		}
 	}
+
+	return nil
 }
